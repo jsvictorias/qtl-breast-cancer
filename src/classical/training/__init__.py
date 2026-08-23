f475973ef97d31cd1a5cfe613a2f9e914c30df7d
jsvictoria <juliasantos7652@gmail.com>
2026-08-22 21:17:48 -0300

    new: clear template

--- src/classical/training/__init__.py
+++
@@ -1,4 +0,0 @@
-from classical.training.metrics import compute_classification_metrics
-from classical.training.train import fit_model, predict_model
-
-__all__ = ["fit_model", "predict_model", "compute_classification_metrics"]
--- src/classical/transforms/__init__.py
+++
@@ -1,3 +0,0 @@
-from classical.transforms.transforms import build_lenet_transform
-
-__all__ = ["build_lenet_transform"]
